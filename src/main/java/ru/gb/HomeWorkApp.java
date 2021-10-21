dffc59d5405dc9aa8f3abbac7892e192a87c8f06
slavak86 <slavak86@yandex.ru>
2021-10-21 15:27:59 +0500

    The first task done

---
+++ src/main/java/ru/gb/HomeWorkApp.java
@@ -0,0 +1,16 @@
+package ru.gb;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+    public static void printThreeWords()  {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+
+}
