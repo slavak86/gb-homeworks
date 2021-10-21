7f4485a447dfe96274570acad3bae6facaa215f1
slavak86 <slavak86@yandex.ru>
2021-10-21 16:02:00 +0500

    The second task done

--- src/main/java/ru/gb/Homework2.java
+++ src/main/java/ru/gb/Homework2.java
@@ -3,10 +3,19 @@ package ru.gb;
 public class Homework2 {
     public static void main(String[] args) {
         System.out.println(with10to20(10, 20));
+        isPositiveOrNegative(-2);
     }
 
     private static boolean with10to20(int number1, int number2) {
         return (number1 + number2) >= 10 && (number1 + number2) <= 20;
 
+    }
+
+    private static void isPositiveOrNegative(int x) {
+        if (x >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
     }
 }
