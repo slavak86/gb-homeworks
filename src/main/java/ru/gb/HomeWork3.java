4384233c28f4879295f22b15e8aaeb8afb90b156
slavak86 <slavak86@yandex.ru>
2021-10-24 14:58:40 +0500

    Completed all required tasks

---
+++ src/main/java/ru/gb/HomeWork3.java
@@ -0,0 +1,65 @@
+package ru.gb;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        invertArray();
+        System.out.println(); //Перенос на новую строку второе задание.
+        fillArray();
+        changeArray();
+        fillDiagonal();
+        retLenArr(10, 7);
+    }
+
+    public static void invertArray() {
+        int array[] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+            System.out.printf("%d ", array[i]);
+
+        }
+    }
+
+    public static void fillArray() {
+        int array[] = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+            System.out.printf("array[%02d] = %d\n", i, array[i]);
+        }
+    }
+
+    public static void changeArray() {
+        int array[] = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] = array[i] * 2;
+            }
+            System.out.printf("%d ", array[i]);
+        }
+    }
+
+    public static void fillDiagonal() {
+        int array[][] = new int[5][5];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0, x = array[i].length; j < array[i].length; j++, x--) {
+                if (i == j || i == x - 1) array[i][j] = 1;
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void retLenArr(int len, int initialValue){
+        int array[] = new int[len];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = initialValue;
+            System.out.printf("%d ", array[i]);
+        }
+        return;
+
+    }
+}
+
